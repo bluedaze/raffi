d264e0bd1974aa2bf0d8207f5dc7f857c3b0ea03
Sean <seanpedigo@gmail.com>
2022-05-06 11:33:25 -0400

    Fixed cronjob to send message for weekly meeting

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+#!/usr/bin/env node
 const cron = require('cron')
 const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
-const { SlashCommandBuilder } = require('@discordjs/builders');
 
-const job = cron.job(
-	'0 10 * * 1', 
-	() => console.log('Message every minute'),
-	null,
-	true,
-	'America/New_York'
-)
-job.start()
+console.log("Starting...")
+
+client.on("ready", () => {
+	const guild = client.guilds.cache.get("971439306763800586")
+	const channel = guild.channels.cache.get('971439306763800589'); 
+	var job = cron.job(
+		'0 10 * * 1',
+		function() {
+			channel.send("Rise and shine, it's time for another zoom extravaganza!");
+			channel.send("Come join at the following link: https://us02web.zoom.us/j/86054725345?pwd=S0s3RmpaMjM4NW9SOU01TWEyTzRCdz09");
+		},
+		null,
+		true,
+		'America/New_York'
+	);
+});
 
 const prefix = "!"
 client.on("messageCreate", function(message) {
@@ -21,14 +29,6 @@ client.on("messageCreate", function(message) {
 	const commandBody = message.content.slice(prefix.length);
 	const args = commandBody.split(' ');
 	const command = args.shift().toLowerCase();
-	console.log("message");
-	console.log(message);
-	console.log("Command Body:");
-	console.log(commandBody);
-	console.log("Command:" );
-	console.log(command);
-	console.log("Args:" );
-	console.log(args);
 	if (command === "ping"){
 		const timeTaken = Date.now() - message.createdTimestamp;
 		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
@@ -36,6 +36,7 @@ client.on("messageCreate", function(message) {
 	else if (command === "zoom"){
 		const link = "https://us02web.zoom.us/j/86054725345?pwd=S0s3RmpaMjM4NW9SOU01TWEyTzRCdz09";
 		message.author.send(link);
+		message.reply("Direct message with zoom link has been sent to you.")
 	}
 	else if (command === "meeting"){
 		message.reply("Monday, 10am");
